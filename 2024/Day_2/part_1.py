0187d10621ddd4f9784e2fb1105b0b772e70fefb
gwenmich <79330147+cgmich@users.noreply.github.com>
2025-01-02 21:51:29 +0000

    Day 2 part 1 attempt

---
+++ 2024/Day_2/part_1.py
@@ -0,0 +1,30 @@
+
+reports = []
+
+with open("puzzle_2_input.txt", "r") as file:
+    for line in file:
+        levels = line.split()
+        reports.append(levels)
+
+    file.close()
+
+
+def safe_reports(input):
+    # find if numbers are ascending or descending
+    safe_reps = []
+    for report in input:
+        if report == sorted(report, reverse = True):
+            safety = all(int(num1) - int(num2) in range(1,4) for num1, num2 in zip(report[:-1], report[1:]))
+            safe_reps.append(safety)
+
+        elif report == sorted(report, reverse = False):
+            safety = all(int(num2) - int(num1) in range(1,4) for num1, num2 in zip(report[:-1], report[1:]))
+            safe_reps.append(safety)
+
+        else:
+            safe_reps.append(False)
+    print(safe_reps)
+    return safe_reps.count(True)
+
+print(safe_reports(reports))
+
